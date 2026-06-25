a38fbe8bceb80515cfe2c7333ae02506f39d55f6
Karl2310 <108998526+Karl2310@users.noreply.github.com>
2026-06-25 14:31:33 -0400

    "Configuracion de conexion a base de datos y variables de entorno"

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2/promise');
 // Configuración de la conexión a la base de datos
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'databasePeliculas',
-  port: 3306, // Puerto por defecto de MySQL
-  password: '',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  password: process.env.DB_PASSWORD,
 });
 
 module.exports = pool;
